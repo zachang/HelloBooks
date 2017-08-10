11d696ad9fcca8e96cf080b846fb22caa94a2f90
zachang <zachangdawuda@gmail.com>
2017-08-10 01:09:10 +0100

    evamp

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DATABASE_URL,
+    port: 5432,
+    dialect: 'postgres'
+  },
   development: {
     username: process.env.DB_USER,
     password: process.env.PASSWORD,
