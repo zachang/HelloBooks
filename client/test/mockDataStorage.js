dba7f807c35545c41a72b5007b324653305350f2
zachang <zachangdawuda@gmail.com>
2018-01-08 15:42:32 +0100

    frontend test setup using jest

---
+++ client/test/mockDataStorage.js
@@ -0,0 +1,19 @@
+let sessionStorage = {};
+
+export default {
+  setItem(key, value) {
+    return {
+      ...sessionStorage,
+      key: value
+    };
+  },
+  getItem(key) {
+    return sessionStorage[key];
+  },
+  removeItem(key) {
+    return delete sessionStorage[key];
+  },
+  clear() {
+    sessionStorage = {};
+  }
+};
